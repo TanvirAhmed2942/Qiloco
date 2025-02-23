0e6f012fcaefa66a74b89d1a93e3b81eb0388057
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-02-23 18:25:15 +0600

    before leaving office

--- src/Layout/Main/Sidebar.jsx
+++ src/Layout/Main/Sidebar.jsx
@@ -2,20 +2,15 @@
 // import React, { useEffect, useState } from "react";
 // import { Link, useLocation, useNavigate } from "react-router-dom";
 
-// import { TbBellBolt, TbDashboard, TbDatabaseDollar } from "react-icons/tb";
-
+// import { TbBellBolt, TbDashboard } from "react-icons/tb";
 // import { RxDashboard } from "react-icons/rx";
-
 // import { PiWallet } from "react-icons/pi";
-
-// import { PiCirclesThreePlusLight } from "react-icons/pi";
-// import { RiSettings5Line } from "react-icons/ri";
 // import { FiLogOut, FiUsers } from "react-icons/fi";
-// import { FaHandsHelping } from "react-icons/fa";
-// import { FaDiagramProject } from "react-icons/fa6";
+// // import { FaHandsHelping, FaDiagramProject } from "react-icons/fa6";
 // import { CgTemplate } from "react-icons/cg";
+// import { RiSettings5Line } from "react-icons/ri";
 
-// const Sidebar = () => {
+// const Sidebar = ({ isCollapsed }) => {
 //   const location = useLocation();
 //   const path = location.pathname;
 //   const [selectedKey, setSelectedKey] = useState("");
@@ -33,27 +28,34 @@
 //       icon: <RxDashboard size={24} />,
 //       label: <Link to="/">Overview</Link>,
 //     },
-
-//     {
-//       key: "/serviceproviders",
-//       icon: <FaHandsHelping size={24} />,
-//       label: <Link to="/serviceproviders">Service Provider</Link>,
-//     },
-
+//     // {
+//     //   key: "/serviceproviders",
+//     //   icon: <FaHandsHelping size={24} />,
+//     //   label: <Link to="/serviceproviders">Service Provider</Link>,
+//     // },
 //     {
 //       key: "/transaction",
 //       icon: <PiWallet size={25} />,
 //       label: <Link to="/transaction">Transaction</Link>,
 //     },
-
 //     {
 //       key: "/customer",
 //       icon: <FiUsers size={23} />,
 //       label: <Link to="/customer">Customer</Link>,
 //     },
 //     {
+//       key: "/pushnotification",
+//       icon: <TbBellBolt size={24} />,
+//       label: <Link to="/pushnotification">PushNotification</Link>,
+//     },
+//     // {
+//     //   key: "/setting",
+//     //   icon: <RiSettings5Line size={24} />,
+//     //   label: <Link to="/setting">Setting</Link>,
+//     // },
+//     {
 //       key: "subMenuSetting1",
-//       icon: <FaDiagramProject size={23} />,
+//       icon: <RiSettings5Line size={23} />,
 //       label: "Project",
 //       children: [
 //         {
@@ -82,123 +84,43 @@
 //         },
 //       ],
 //     },
-
-//     {
-//       key: "/pushnotification",
-//       icon: <TbBellBolt size={24} />,
-//       label: <Link to="/pushnotification">PushNotification</Link>,
-//     },
-
-//     {
-//       key: "subMenuSetting",
-//       icon: <CgTemplate size={24} />,
-//       label: "Cms",
-//       children: [
-
-//         {
-//           key: "/privacy-policy",
-//           label: (
-//             <Link to="/privacy-policy" className="text-white hover:text-white">
-//               Privacy Policy
-//             </Link>
-//           ),
-//         },
-//         {
-//           key: "/terms-and-conditions",
-//           label: (
-//             <Link
-//               to="/terms-and-conditions"
-//               className="text-white hover:text-white"
-//             >
-//               Terms And Condition
-//             </Link>
-//           ),
-//         },
-//         {
-//           key: "/faq",
-//           label: (
-//             <Link to="/faq" className="text-white hover:text-white">
-//               FAQ
-//             </Link>
-//           ),
-//         },
-//         {
-//           key: "/contact",
-//           label: (
-//             <Link to="/contact" className="text-white hover:text-white">
-//               Contact Us
-//             </Link>
-//           ),
-//         },
-
-//       ],
-//     },
-
-//     {
-//       key: "/setting",
-//       icon: <RiSettings5Line size={24} />,
-//       label: <Link to="/setting">Setting</Link>,
-//     },
 //     {
 //       key: "/logout",
-//       icon: <FiLogOut size={24} color="white" />,
+//       icon: <FiLogOut size={24} />,
 //       label: (
 //         <p onClick={handleLogout} className="text-white hover:text-white">
 //           Logout
 //         </p>
 //       ),
 //     },
-
 //   ];
 
 //   useEffect(() => {
-//     const selectedItem = menuItems.find(
-//       (item) =>
-//         item.key === path || item.children?.some((sub) => sub.key === path)
-//     );
-
-//     if (selectedItem) {
-//       setSelectedKey(path);
-
-//       if (selectedItem.children) {
-//         setOpenKeys([selectedItem.key]);
-//       } else {
-//         const parentItem = menuItems.find((item) =>
-//           item.children?.some((sub) => sub.key === path)
-//         );
-//         if (parentItem) {
-//           setOpenKeys([parentItem.key]);
-//         }
-//       }
-//     }
+//     setSelectedKey(path);
 //   }, [path]);
 
-//   const handleOpenChange = (keys) => {
-//     setOpenKeys(keys);
-//   };
-
 //   return (
-//     <div className="w-auto bg-prince  mt-20  h-[full]">
-//       <Link
-//         to={"/"}
-//         className=" flex items-center justify-center py-4 text-white"
-//       >
-//         <div className="w-auto flex items-center justify-center bg-prince border border-gtdandy px-10 py-3 gap-5 rounded-lg">
-//           <TbDashboard size={30} className=" text-white" />
-//           <p className="text-4xl font-semibold text-[18px] font-sans tracking-wider text-white">
-//             Dashboard
-//           </p>
+//     <div
+//       className={`bg-quilocoP h-full shadow-md transition-all duration-300 ${
+//         isCollapsed ? "w-[200px]" : "w-[280px]"
+//       }`}
+//     >
+//       <Link to="/" className="flex items-center justify-center py-4 text-white">
+//         <div className="w-full flex items-center justify-center bg-quilocoP border border-gtdandy px-4 py-3 gap-3 rounded-lg">
+//           <TbDashboard size={30} className="text-white" />
+//           {!isCollapsed && <p className="text-xl font-semibold">Dashboard</p>}
 //         </div>
 //       </Link>
 
 //       <Menu
 //         mode="inline"
 //         selectedKeys={[selectedKey]}
-//         openKeys={openKeys}
-//         onOpenChange={handleOpenChange}
-//         style={{ borderRightColor: "transparent", background: "#975cdb" }}
-//         items={menuItems}
-//         className="space-y-5 text-white mt-10"
+//         style={{ background: "#232323" }}
+//         items={menuItems.map((item) => ({
+//           ...item,
+//           label: isCollapsed ? <span>{item.icon}</span> : item.label,
+//         }))}
+//         className="text-white mt-10"
 //       />
 //     </div>
 //   );
@@ -214,9 +136,8 @@ import { TbBellBolt, TbDashboard } from "react-icons/tb";
 import { RxDashboard } from "react-icons/rx";
 import { PiWallet } from "react-icons/pi";
 import { FiLogOut, FiUsers } from "react-icons/fi";
-// import { FaHandsHelping, FaDiagramProject } from "react-icons/fa6";
-import { CgTemplate } from "react-icons/cg";
 import { RiSettings5Line } from "react-icons/ri";
+import qilocoLogo from "../../assets/quiloco/qilocoLogo.png";
 
 const Sidebar = ({ isCollapsed }) => {
   const location = useLocation();
@@ -234,68 +155,68 @@ const Sidebar = ({ isCollapsed }) => {
     {
       key: "/",
       icon: <RxDashboard size={24} />,
-      label: <Link to="/">Overview</Link>,
+      label: isCollapsed ? null : <Link to="/">Overview</Link>,
     },
-    // {
-    //   key: "/serviceproviders",
-    //   icon: <FaHandsHelping size={24} />,
-    //   label: <Link to="/serviceproviders">Service Provider</Link>,
-    // },
     {
       key: "/transaction",
       icon: <PiWallet size={25} />,
-      label: <Link to="/transaction">Transaction</Link>,
+      label: isCollapsed ? null : <Link to="/transaction">Transaction</Link>,
     },
     {
       key: "/customer",
       icon: <FiUsers size={23} />,
-      label: <Link to="/customer">Customer</Link>,
+      label: isCollapsed ? null : <Link to="/customer">Customer</Link>,
     },
     {
       key: "/pushnotification",
       icon: <TbBellBolt size={24} />,
-      label: <Link to="/pushnotification">PushNotification</Link>,
-    },
-    {
-      key: "/setting",
-      icon: <RiSettings5Line size={24} />,
-      label: <Link to="/setting">Setting</Link>,
+      label: isCollapsed ? null : (
+        <Link to="/pushnotification">PushNotification</Link>
+      ),
     },
     {
       key: "subMenuSetting1",
       icon: <RiSettings5Line size={23} />,
-      label: "Project",
-      children: [
-        {
-          key: "/newrequest",
-          label: (
-            <Link to="/newrequest" className="text-white">
-              New Request
-            </Link>
-          ),
-        },
-        {
-          key: "/activeproject",
-          label: (
-            <Link to="/activeproject" className="text-white hover:text-white">
-              Active Project
-            </Link>
-          ),
-        },
-        {
-          key: "/pendingproject",
-          label: (
-            <Link to="/pendingproject" className="text-white hover:text-white">
-              Pending Project
-            </Link>
-          ),
-        },
-      ],
+      label: isCollapsed ? null : "Project",
+      children: isCollapsed
+        ? []
+        : [
+            {
+              key: "/newrequest",
+              label: (
+                <Link to="/newrequest" className="text-white">
+                  New Request
+                </Link>
+              ),
+            },
+            {
+              key: "/activeproject",
+              label: (
+                <Link
+                  to="/activeproject"
+                  className="text-white hover:text-white"
+                >
+                  Active Project
+                </Link>
+              ),
+            },
+            {
+              key: "/pendingproject",
+              label: (
+                <Link
+                  to="/pendingproject"
+                  className="text-white hover:text-white"
+                >
+                  Pending Project
+                </Link>
+              ),
+            },
+          ],
     },
     {
       key: "/logout",
       icon: <FiLogOut size={24} />,
-      label: (
+      label: isCollapsed ? null : (
         <p onClick={handleLogout} className="text-white hover:text-white">
           Logout
         </p>
@@ -314,9 +235,10 @@ const Sidebar = ({ isCollapsed }) => {
       }`}
     >
       <Link to="/" className="flex items-center justify-center py-4 text-white">
-        <div className="w-full flex items-center justify-center bg-quilocoP border border-gtdandy px-4 py-3 gap-3 rounded-lg">
-          <TbDashboard size={30} className="text-white" />
-          {!isCollapsed && <p className="text-xl font-semibold">Dashboard</p>}
+        <div className="w-full flex items-center justify-center bg-quilocoP px-4 py-3 gap-3 rounded-lg">
+          {/* <TbDashboard size={30} className="text-white" />
+          {!isCollapsed && <p className="text-xl font-semibold">Dashboard</p>} */}
+          <img src={qilocoLogo} />
         </div>
       </Link>
 
@@ -324,10 +246,7 @@ const Sidebar = ({ isCollapsed }) => {
         mode="inline"
         selectedKeys={[selectedKey]}
         style={{ background: "#232323" }}
-        items={menuItems.map((item) => ({
-          ...item,
-          label: isCollapsed ? <span>{item.icon}</span> : item.label,
-        }))}
+        items={menuItems}
         className="text-white mt-10"
       />
     </div>
